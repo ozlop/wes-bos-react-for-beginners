07f7f25169aadfc0830f38645b73790604f439cd
ozlop <oswaldo.t.lopez@gmail.com>
2019-08-12 21:53:57 -0700

    Add store page reroute
    Used an onSubmit event to trigger a custom component method that
    retrieves the store name and uses history.push to redirect the
    application.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,25 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = event => {
+        // 1. Stop the form from submitting
+        event.preventDefault();
+        // 2. Get the text from the input
+        const storeName = this.myInput.current.value;
+        // 3. Change the page the /store/<input>
+        this.props.history.push(`/store/${storeName}`)
+    };
+
     render() {
         return (
-            <form action="" className="store-selector">
+            <form action="" className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
                 <input
                     type="text"
+                    ref={this.myInput}
                     required placeholder="Store Name"
                     defaultValue={getFunName()}
                 />

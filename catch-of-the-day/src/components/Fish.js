e7290bde733c040b8d343e2286640b8d17d6832e
ozlop <oswaldo.t.lopez@gmail.com>
2019-08-18 11:36:32 -0700

    Add App order state update on Fish button click
    The addToOrder app method is passed into the Fish component using props
    where it is used on the add to cart button.

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -13,8 +13,7 @@ class Fish extends React.Component {
                 <span className="price">{formatPrice(price)}</span>
                 </h3>
                 <p>{desc}</p>
-                <button
-                    disabled={!isAvailable}>
+                <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
                     {isAvailable ? 'Add To Order' : 'Sold Out!'}
                 </button>
             </li>
